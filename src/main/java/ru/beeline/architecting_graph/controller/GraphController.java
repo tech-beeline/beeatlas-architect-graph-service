28d9e2b0fde9e99e643d0cc146688247b658db9f
salikhanov <SAlikhanov@oms.beeline.ru>
2025-06-23 19:06:11 +0600

    fix graph construct

--- src/main/java/ru/beeline/architecting_graph/controller/GraphController.java
+++ src/main/java/ru/beeline/architecting_graph/controller/GraphController.java
@@ -1,16 +1,10 @@
 package ru.beeline.architecting_graph.controller;
 
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import ru.beeline.architecting_graph.config.RestConfig;
 import ru.beeline.architecting_graph.service.compareVersions.CompareVersions;
 import ru.beeline.architecting_graph.service.createDiagrams.CreateDiagrams;
@@ -40,52 +34,52 @@ public class GraphController {
     }
 
     @PostMapping("/graph/local/{docId}")
-    @ApiOperation("Пересоздание локального графа, используя документ, в котором описывается система (все вершины и связи помечаются graphTag: Local)")
+    @Operation(summary = "Пересоздание локального графа, используя документ, в котором описывается система (все вершины и связи помечаются graphTag: Local)")
     public ResponseEntity<String> LocalGraph(@PathVariable("docId") Long docId) {
         return graphConstruction.graphConstruct(docId, "Local");
     }
 
     @PostMapping("/graph/{docId}")
-    @ApiOperation("Добавление системы из указанного документа в глобальный граф (все вершины и связи помечаются graphTag: Global)")
+    @Operation(summary = "Добавление системы из указанного документа в глобальный граф (все вершины и связи помечаются graphTag: Global)")
     public ResponseEntity<String> GlobalGraph(@PathVariable("docId") Long docId) {
         return graphConstruction.graphConstruct(docId, "Global");
     }
 
     @GetMapping("/context/{softwareSystemMnemonic}/{containerMnemonic}")
-    @ApiOperation("Генерация json с описанием containerView")
+    @Operation(summary = "Генерация json с описанием containerView")
     public ResponseEntity<String> getC4Diagramm(@PathVariable String softwareSystemMnemonic,
-            @PathVariable(required = false) String containerMnemonic) {
+                                                @PathVariable(required = false) String containerMnemonic) {
 
         return createDiagrams.createDiagramm(autorization, softwareSystemMnemonic, containerMnemonic, null);
     }
 
     @GetMapping("/context/{softwareSystemMnemonic}")
-    @ApiOperation("Генерация json с описанием contextView")
+    @Operation(summary = "Генерация json с описанием contextView")
     public ResponseEntity<String> getContextDiagramm(@PathVariable String softwareSystemMnemonic) {
         return getC4Diagramm(softwareSystemMnemonic, null);
     }
 
     @GetMapping("/deployment/{environment}/{softwareSystemMnemonic}")
-    @ApiOperation("Генерация json с описанием deploymentView")
+    @Operation(summary = "Генерация json с описанием deploymentView")
     public ResponseEntity<String> getDeploymentDiagramm(@PathVariable String environment,
-            @PathVariable String softwareSystemMnemonic) {
+                                                        @PathVariable String softwareSystemMnemonic) {
 
         return createDiagrams.createDiagramm(autorization, softwareSystemMnemonic, null, environment);
     }
 
     @GetMapping("/diff/{cmdb}/{firstVersion}/{secondVersion}")
-    @ApiOperation("Сравнение двух версий указанной системы")
+    @Operation(summary = "Сравнение двух версий указанной системы")
     public ResponseEntity<String> compareVersions(@PathVariable String cmdb,
-            @PathVariable Integer firstVersion,
-            @PathVariable(required = false) Integer secondVersion) {
+                                                  @PathVariable Integer firstVersion,
+                                                  @PathVariable(required = false) Integer secondVersion) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(compareVersions.compareVersion(cmdb, firstVersion, secondVersion));
     }
 
     @GetMapping("/diff/{cmdb}/{firstVersion}")
-    @ApiOperation("Сравнение указанной версии системы с текущей (последней/актуальной)")
+    @Operation(summary = "Сравнение указанной версии системы с текущей (последней/актуальной)")
     public ResponseEntity<String> compareWithCur(@PathVariable String cmdb,
-            @PathVariable Integer firstVersion) {
+                                                 @PathVariable Integer firstVersion) {
         return ResponseEntity.status(HttpStatus.OK).body(compareVersions.compareVersion(cmdb, firstVersion, null));
     }
 
