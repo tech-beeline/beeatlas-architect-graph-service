3c830f682f2d75380626ebcb313a8439975d887b
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-07-07 14:03:37 +0300

    fix swagger

--- src/main/java/ru/beeline/architecting_graph/config/SwaggerConfig.java
+++ src/main/java/ru/beeline/architecting_graph/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package ru.beeline.architecting_graph.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +14,28 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("Architect Graph Service API")
-                        .version("1.0")
-                        .description("API документация"));
+                              .title("Architect Graph Service API")
+                              .version("1.0")
+                              .description("API документация")
+                              .contact(new Contact()
+                                               .name("Example")
+                                               .url("https://www.example.com")
+                                               .email("example@company.com"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                                    .addSecuritySchemes(securitySchemeName,
+                                                        new SecurityScheme()
+                                                                .name(securitySchemeName)
+                                                                .type(SecurityScheme.Type.HTTP)
+                                                                .scheme("bearer")
+                                                                .bearerFormat("JWT")
+                                    )
+                );
     }
 }
+
