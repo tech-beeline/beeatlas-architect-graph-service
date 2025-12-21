a6d4973d68dca76aeb3c741f0ce271fb58a76bcb
salikhanov <SAlikhanov@oms.beeline.ru>
2025-12-21 14:04:53 +0600

    SFDM-3205 добавлен флаг use-doc-service. Для подключения к rabbit и reddis.

--- src/main/java/ru/beeline/architecting_graph/controller/GraphController.java
+++ src/main/java/ru/beeline/architecting_graph/controller/GraphController.java
@@ -6,9 +6,11 @@ package ru.beeline.architecting_graph.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.beeline.architecting_graph.dto.*;
 import ru.beeline.architecting_graph.exception.ConflictValuesException;
 import ru.beeline.architecting_graph.service.compareVersions.CompareVersionsService;
@@ -68,19 +70,32 @@ public class GraphController {
     @GetMapping("/graph/{graph-type}/task/{task-id}")
     @Operation(summary = "Получение статуса графа по taskKey и типу графа")
     public ResponseEntity<TaskCacheDTO> getGraphByTask(@PathVariable("graph-type") String graphType,
-                                                       @PathVariable("task-id") String taskId) {
+                                                       @PathVariable("task-id") String taskId,
+                                                       @Value("${app.feature.use-doc-service:false}")
+                                                       boolean isDocServiceEnabled) {
+        if (!isDocServiceEnabled) {
+            throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED);
+        }
         return graphConstructionService.getGraphByTask(graphType, taskId);
     }
 
-    @PostMapping("/graph/local/json")
+    @PostMapping("/graph/local/{json}")
     @Operation(summary = "Пересоздание локального графа, используя документ, в котором описывается система (все вершины и связи помечаются graphTag: Local)")
-    public ResponseEntity<String> LocalGraph(@RequestBody String json) {
+    public ResponseEntity<String> LocalGraph(@PathVariable("json") String json,
+                                             @Value("${app.feature.use-doc-service:false}") boolean isDocServiceEnabled) {
+        if (!isDocServiceEnabled) {
+            throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED);
+        }
         return graphConstructionService.graphConstruct(json, "Local");
     }
 
-    @PostMapping("/graph/json")
+    @PostMapping("/graph/{json}")
     @Operation(summary = "Добавление системы из указанного документа в глобальный граф (все вершины и связи помечаются graphTag: Global)")
-    public ResponseEntity<String> GlobalGraph(@RequestBody String json) {
+    public ResponseEntity<String> GlobalGraph(@PathVariable("json") String json,
+                                              @Value("${app.feature.use-doc-service:false}") boolean isDocServiceEnabled) {
+        if (!isDocServiceEnabled) {
+            throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED);
+        }
         return graphConstructionService.graphConstruct(json, "Global");
     }
 
@@ -99,8 +114,8 @@ public class GraphController {
     @GetMapping("/graph/deployment/{cmdb}/influence")
     @Operation(summary = "Метод для получения связанных систем деплоймента")
     public ResponseEntity<ProductInfluenceDTO> getDeploymentInfluence(@PathVariable String cmdb,
-                                                            @RequestParam String name,
-                                                            @RequestParam String env) {
+                                                                      @RequestParam String name,
+                                                                      @RequestParam String env) {
         try {
             return ResponseEntity.ok(productInfluenceService.getDeploymentRelatedSystems(cmdb, name, env));
         } catch (NoSuchElementException e) {
