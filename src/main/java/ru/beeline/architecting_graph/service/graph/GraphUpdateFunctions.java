f2d4a28e71024b96a0094235247c589b22d24f12
Konstantin <KYuSamsonov@tum.beeline.ru>
2026-02-06 12:13:23 +0300

    SFDM-3323 При построении графов все ноды создаются с endVersion

--- src/main/java/ru/beeline/architecting_graph/service/graph/GraphUpdateFunctions.java
+++ src/main/java/ru/beeline/architecting_graph/service/graph/GraphUpdateFunctions.java
@@ -56,7 +56,6 @@ public class GraphUpdateFunctions {
         updateDeploymentNodes(graphTag, model, softwareSystem.getId(), cmdb, curVersion, objects);
         //прокидываем остаточные связи
         updateDeploymentNodeRelationships(graphTag, model, curVersion, cmdb, objects);
-        setEndVersion(graphTag, cmdb, curVersion);
 
     }
 
